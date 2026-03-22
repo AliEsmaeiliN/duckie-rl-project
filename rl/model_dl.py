b4ad535b8ef13df45cb9bf66762dff407350519c
AliEsmaeiliN <ali.n.esmaeilinasab@gmail.com>
2026-03-22 11:37:08 +0100

    Adding the modul to download the artifact from wandb servers

--- rl/model_dl.py
+++ rl/model_dl.py
@@ -1,9 +1,10 @@
 import wandb
 import torch
+#wandb api: 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
 
 api = wandb.Api()
-
-artifact = api.artifact("ali-esm-unipd/DT_RL/run-7hwtyxw0-history:v0")
+model_address = input("Paste the wandb artifact address: ")
+artifact = api.artifact(model_address)
 artifact_dir = artifact.download()
 
 model_path = f"{artifact_dir}/sac_step_latest.cleanrl_model"
