6cae11ca2f3b8b08526effd0eb85a4882d48f7e2
AliEsmaeiliN <ali.n.esmaeilinasab@gmail.com>
2026-03-24 15:47:17 +0000

    adding the job file

---
+++ job_duckie.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=duckie_rl
+#SBATCH --output=output/duckie_%j.out
+#SBATCH -e output/duckie_%j.err
+#SBATCH --time=20:00:00
+#SBATCH --partition=pgpu_most
+#SBATCH --account=dei_most
+#SBATCH --gpus=1
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=4
+
+# --- 1. Environment Setup ---
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate duckie-rl
+
+# --- 2. Path Configuration ---
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
+if [ ! -f $CONDA_PREFIX/lib/libtiff.so.5 ]; then
+    ln -s $CONDA_PREFIX/lib/libtiff.so.6 $CONDA_PREFIX/lib/libtiff.so.5
+fi
+
+# --- 4. Launch Training ---
+python rl/td3_continuous_action.py \
+    --seed 1 \
+    --env-id FastR_V3_withDR \
+    --total-timesteps 1000001 \
+    --track \
+    --run-notes ""
