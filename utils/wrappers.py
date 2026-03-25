7173e3d74dad87b8fa4a8ef77d32c5bd931a6f7c
AliEsmaeiliN <ali.n.esmaeilinasab@gmail.com>
2026-03-25 16:06:42 +0100

    Minor changes

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -6,35 +6,35 @@ from PIL import Image
 from gym_duckietown.simulator import Simulator
 
 
-class MotionBlurWrapper(Simulator):
-    def __init__(self, env=None):
-        Simulator.__init__(self)
-        self.env = env
-        self.frame_skip = 3
-        self.env.delta_time = self.env.delta_time / self.frame_skip
-
+class MotionBlurWrapper(gym.Wrapper):
+    def __init__(self, env=None, frame_skip=3):
+        super().__init__(env)
+        self.frame_skip = frame_skip
+        self.env.unwrapped.delta_time = self.env.unwrapped.delta_time / (self.frame_skip + 1)
+        
+        
+        self.weights = [0.01, 0.04, 0.15, 0.8]
     def step(self, action: np.ndarray):
-        action = np.clip(action, -1, 1)
-        # Actions could be a Python list
-        action = np.array(action)
+        action = np.clip(action, self.action_space.low, self.action_space.high)
         motion_blur_window = []
+
         for _ in range(self.frame_skip):
-            obs = self.env.render_obs()
+            obs = self.env.unwrapped.render_obs()
             motion_blur_window.append(obs)
-            self.env.update_physics(action)
+            self.env.unwrapped.update_physics(action)
 
-        # Generate the current camera image
 
-        obs = self.env.render_obs()
+        obs = self.env.unwrapped.render_obs()
         motion_blur_window.append(obs)
-        obs = np.average(motion_blur_window, axis=0, weights=[0.8, 0.15, 0.04, 0.01])
 
-        misc = self.env.get_agent_info()
+        # axis=0 averages the pixel values across the time dimension
+        blurred_obs = np.average(motion_blur_window, axis=0, weights=self.weights).astype(np.uint8)
 
-        d = self.env._compute_done_reward()
-        misc["Simulator"]["msg"] = d.done_why
+        d_info = self.env.unwrapped._compute_done_reward(action)
+        misc = self.env.unwrapped.get_agent_info()
+        misc["Simulator"]["msg"] = d_info.done_why
 
-        return obs, d.reward, d.done, misc
+        return blurred_obs, d_info.reward, d_info.done, False, misc
 
 
 class ResizeWrapper(gym.ObservationWrapper):
